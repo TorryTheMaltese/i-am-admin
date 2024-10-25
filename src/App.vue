625b237d8ea9d93a47d3f67ec13fdbe49674cbe1
unknown <torryiscute@gmail.com>
2024-10-25 11:43:01 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <GetAdminAPI />
+</template>
+
+<script>
+import GetAdminAPI from './components/GetAdminAPI.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GetAdminAPI
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GetAdminAPI.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "GetAdminAPI",
+  data() {
+    return {};
+  },
+  methods: {
+    getUsers() {
+      const path = `https://admin.googleapis.com`;
+      let userInfo = axios.create();
+
+      userInfo
+        .get(path + "/admin/directory/v1/users")
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log("err : ", err);
+        });
+    },
+  },
+  created() {
+    this.getUsers();
+  },
+};
+</script>
+<style></style>
